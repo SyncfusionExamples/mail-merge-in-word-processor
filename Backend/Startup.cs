c0a57e6b024d2b6f6ac0875445941c6268d140db
Suriya.Murugan <suriya.murugan@syncfusion.com>
2022-10-13 15:56:02 +0530

    Updated sample in Angular platform

---
+++ Backend/Startup.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.Edm;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.OData.UriParser;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using EJ2APIServices.Models;
+using EJ2APIServices.Controllers;
+using Syncfusion.EJ2.SpellChecker;
+using System.IO;
+using Newtonsoft.Json;
+
+
+namespace EJ2APIServices
+{
+    public class Startup
+    {
+        private string _contentRootPath = "";
+        internal static List<DictionaryData> spellDictCollection;
+        internal static string path;
+        internal static string personalDictPath;
+
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+            _contentRootPath = env.ContentRootPath;
+
+            path = Configuration["SPELLCHECK_DICTIONARY_PATH"];
+            string jsonFileName = Configuration["SPELLCHECK_JSON_FILENAME"];
+            //check the spell check dictionary path environment variable value and assign default data folder
+            //if it is null.
+            path = string.IsNullOrEmpty(path) ? Path.Combine(env.ContentRootPath, "App_Data") : Path.Combine(env.ContentRootPath, path);
+            //Set the default spellcheck.json file if the json filename is empty.
+            jsonFileName = string.IsNullOrEmpty(jsonFileName) ? Path.Combine(path, "spellcheck.json") : Path.Combine(path, jsonFileName);
+            if (System.IO.File.Exists(jsonFileName))
+            {
+                string jsonImport = System.IO.File.ReadAllText(jsonFileName);
+                List<DictionaryData> spellChecks = JsonConvert.DeserializeObject<List<DictionaryData>>(jsonImport);
+                spellDictCollection = new List<DictionaryData>();
+                //construct the dictionary file path using customer provided path and dictionary name
+                foreach (var spellCheck in spellChecks)
+                {
+                    spellDictCollection.Add(new DictionaryData(spellCheck.LanguadeID, Path.Combine(path, spellCheck.DictionaryPath), Path.Combine(path, spellCheck.AffixPath)));
+                    personalDictPath = Path.Combine(path, spellCheck.PersonalDictPath);
+                }
+            }
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOData();
+            services.AddMvc().AddJsonOptions(x => {
+                x.SerializerSettings.ContractResolver = null;
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+                });
+            });
+            string connection = Configuration.GetConnectionString("Test");
+            if (connection.Contains("%CONTENTROOTPATH%"))
+            {
+                connection = connection.Replace("%CONTENTROOTPATH%", _contentRootPath);
+
+            }
+            services.AddEntityFrameworkNpgsql().AddDbContext<EEJ2SERVICEEJ2WEBSERVICESSRCAPP_DATADIAGRAMMDFContext>(options => options.UseNpgsql(connection));
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseCors("AllowAllOrigins");
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc(b =>
+            {
+                b.Count().Filter().OrderBy().Expand().Select().MaxTop(null);
+            });
+
+        }
+
+    }
+
+    public class ServerPath
+    {
+        public static string MapPath = "";
+    }
+}
